29a524b905942d7952007146dc18d16a0aa5806e
dungna2401@gmail <dungna2401@gmail.com>
2017-08-24 11:25:26 +0700

    edit dev

--- ser.js
+++ ser.js
@@ -4,3 +4,5 @@ day la lan dau tien toi thuc hanh github nghiem tuic\
 -- day la nhung diong toi them vao tren nhan dev
 
 co the co loi nhung toi nghi la se hoc dc nhieu thuc
+
+-- them dong tu nhanh master
