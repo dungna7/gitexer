945cde5291d609d7ebd511c3706e6552a145328a
dungna2401@gmail <dungna2401@gmail.com>
2017-08-25 15:03:31 +0700

    add

--- ser.js
+++ ser.js
@@ -1,18 +1,2 @@
-
-09h35 sáng nay ngày 24/8/2017 
-mình đến cty laser lấy hàng.
- Lúc xuống lấy xe máy thì thấy c
- ó gói giấy rớt ngay gần dưới gầm xe của mình.
- Nhìn qua mình tưởng ai nhận giấy tờ rồi vứt
- phong bì vỏ lại.
- Vì nhìn quanh không có thùng rác
- nên mình gập làm 3 dắt vào đuôi xe máy.
- Tính gặp thùng rác thì vứt, 
- nhưng loay hoay lại để vào mấy t
- hùng hàng hồi nào không hay.
-aaaaaa
-aaaaaaa
-ccccccc
-bbbbbbb
-bbbbbbb
-bbbbbbb
+aaa
+bbbb
